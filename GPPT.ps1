39431b1e51c1ad187ed61fa135821841800f81ac
JucaGT <60317986+JucaGT@users.noreply.github.com>
2020-01-26 18:06:32 +0100

    Add files via upload

---
+++ GPPT.ps1
@@ -0,0 +1,29 @@
+﻿Clear-Host
+Write-Host "Bienvenido al juego Piedra, Papel o Tijeras"
+$valores=@('PIEDRA', 'PAPEL', 'TIJERAS')
+$maquina=Get-Random $valores.Length
+$jugador = Read-Host "Introduce tu elección en mayúsculas (PIEDRA, PAPEL O TIJERAS)"
+$maquina = $valores[$maquina]
+Write-Host "Elegiste $jugador y la maquina ha elegido $maquina"
+if (($jugador -eq "PIEDRA") -and ($maquina -eq "TIJERAS")){
+    Write-Host "¡Ganaste!"
+} elseif (($jugador -eq "PIEDRA") -and ($maquina -eq "PIEDRA")){
+         Write-Host "¡Empate!"
+    } elseif (($jugador -eq "PIEDRA") -and ($maquina -eq "PAPEL")){
+         Write-Host "¡Perdiste!"
+        } elseif (($jugador -eq "PAPEL") -and ($maquina -eq "PAPEL")){
+            Write-Host "¡Empate!"
+         } elseif (($jugador -eq "PAPEL") -and ($maquina -eq "PIEDRA")){
+                Write-Host "¡Ganaste!"
+            } elseif (($jugador -eq "PAPEL") -and ($maquina -eq "TIJERAS")){
+                 Write-Host "¡Perdiste!"
+                } elseif (($jugador -eq "TIJERAS") -and ($maquina -eq "PAPEL")){
+                    Write-Host "¡Ganaste!"
+                    } elseif (($jugador -eq "TIJERAS") -and ($maquina -eq "PIEDRA")){
+                     Write-Host "¡Perdiste!"
+                     } elseif (($jugador -eq "TIJERAS") -and ($maquina -eq "TIJERAS")){
+                         Write-Host "¡Empate!"
+                        } else {
+                                Write-Host "Has introducido un valor no admitido"
+                            }
+                      
